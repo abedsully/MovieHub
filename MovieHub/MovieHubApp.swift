366bbb0cf69c6af1f0bd440200b9fc4f31d2c544
abedsully <abedsully2909@gmail.com>
2024-06-25 09:02:17 +0700

    Initial Commit

---
+++ MovieHub/MovieHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHubApp.swift
+//  MovieHub
+//
+//  Created by Stefanus Albert Wilson on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
